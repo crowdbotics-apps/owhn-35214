60816557268d6ae10c933957305b9f8ff8c710e7
Crowdbotics <team@crowdbotics.com>
2022-05-14 22:52:25 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/owhn_35214/MainActivity.java
@@ -0,0 +1,15 @@
+package com.owhn_35214;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "owhn_35214";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "owhn_35214",
+  url: "https://owhn_35214.botics.co",
+  api: "https://owhn_35214.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
